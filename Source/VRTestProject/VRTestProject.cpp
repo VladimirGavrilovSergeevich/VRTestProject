1dc6865894ac176b69e5ec7b781c57a101f021c8
VOVAMAINPC <lompooner@gmail.com>
2023-05-18 10:36:27 +0300

    Project creation

---
+++ Source/VRTestProject/VRTestProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VRTestProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRTestProject, "VRTestProject" );
---
+++ Source/VRTestProject/VRTestProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRTestProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRTESTPROJECT_API AVRTestProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
