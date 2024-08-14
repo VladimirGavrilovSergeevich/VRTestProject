708f5e68515b7989b03824f3f31467533c3f31ff
VovaMainPC\Vladimir <lompooner@gmail.com>
2024-08-14 18:18:19 +0300

    init commit after one year. Switching from 4.27 version to 5.4.3

--- Source/VRTestProjectEditor.Target.cs
+++ Source/VRTestProjectEditor.Target.cs
@@ -8,7 +8,7 @@ public class VRTestProjectEditorTarget : TargetRules
 	public VRTestProjectEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "VRTestProject" } );
 	}
 }
